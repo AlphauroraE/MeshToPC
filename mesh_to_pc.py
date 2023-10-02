0677fd2c5806b3fd3d751f7624dfa377b964a294
AlphauroraE <142271610+AlphauroraE@users.noreply.github.com>
2023-10-02 17:38:34 -0400

    Update and rename code.py to mesh_to_pc.py

--- mesh_to_pc.py
+++ mesh_to_pc.py
@@ -1,7 +1,7 @@
 import os
 import open3d as o3d
 
-def mesh_to_pc(inputpath, meshpath,points,outputname,outputpath):
+def mesh_to_pc(inputpath,meshpath,points,outputname,outputpath):
     """"Convert a folder of meshes into a folder of corresponding point clouds"""
     mesh = o3d.io.read_triangle_mesh(meshpath) # Looks at the mesh
     mesh.compute_vertex_normals()
@@ -10,28 +10,29 @@ def mesh_to_pc(inputpath, meshpath,points,outputname,outputpath):
     o3d.io.write_point_cloud(outputname,pcd) # create a file storing the point cloud in the output folder
     os.chdir(inputpath) # move back to the input folder to prepare for the next function call
 
-#TODO: Change inputpath depending on the computer
+#Note: Change inputpath depending on the computer
 inputpath = "..." # The path to the input folder on computer (change depending on computer)
 os.chdir(inputpath) # move into the input folder
 # path = os.getcwd() # get the current working directory (current folder, aka input folder)
 # dir_list = os.listdir(path) # makes a list of the names of the files in my input folder
 
-#TODO: Change parent_dir depending on the computer
+#Note: Change parent_dir depending on the computer
 parent_dir = "..." # This is the place (larger folder) where I want to store my new output folder
 directory = input("What to name the output folder? ") # take user input for what to name the output folder
 outputpath = os.path.join(parent_dir, directory) # combines the two together to make a path to the output folder
-os.mkdir(outputpath) # makes the output folder. now we have a place to store our point clouds!
+os.mkdir(outputpath) # makes the output folder. Now we have a place to store our point clouds!
 
 obj = os.scandir(os.getcwd())
 for entry in obj: # goes through the input folder
     inputfile = entry.name
-    if inputfile != ".DS_Store": # The first entry was always .DS_Store, and this is not a mesh file
-        if inputfile.find("_") != -1: # mesh files without underscore were returning -1 and creating a name cut off letters
+    if inputfile != ".DS_Store": # The first entry is always .DS_Store, and this is not a mesh file, so ignore it
+        if inputfile.find("_") != -1: # mesh files without underscore were returning -1 and creating a name with cut off letters
             newname = inputfile.replace(inputfile[inputfile.find("_"):],"") # basically removes everything after underscore
         else:
             newname = inputfile[:inputfile.find(".ply")] # new name is everything up to the file type specification
         outputfile = newname + ".pcd" # this is the name for the output file of this point cloud
-        mesh_to_pc(inputpath,inputfile,5000,outputfile,outputpath) # call the function defined above
+        #Note: Alter function call as needed
+        mesh_to_pc(inputpath,inputfile,5000,outputfile,outputpath) # call the function defined above. Alter as needed!
 
 os.chdir(outputpath)
 obj = os.scandir(os.getcwd())
